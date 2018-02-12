04e06be328cc7940fd7c483fc93c6c4d22aa696b
Peter Hermansen <hermansenph@gmail.com>
2018-02-12 15:20:03 -0800

    Moved script to main.js #3

--- server/public/main.js
+++ server/public/main.js
@@ -1,1 +1,11 @@
-
+$(function () {
+  var socket = io()
+  $('form').submit(() => {
+    socket.emit('chat message', $('#m').val())
+    $('#m').val('')
+    return false
+  })
+  socket.on('chat message', (msg) => {
+    $('#messages').append($('<li>').text(msg))
+  })
+})
