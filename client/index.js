50c2fb3df251bf0a4deabcfad6026f3da791589e
Peter Hermansen <hermansenph@gmail.com>
2018-02-12 15:38:57 -0800

    Created client/index.js and moved client-side functionality #3

---
+++ client/index.js
@@ -0,0 +1,13 @@
+import io from 'socket.io-client'
+
+$(function () {
+  var socket = io()
+  $('form').submit(() => {
+    socket.emit('chat message', $('#m').val())
+    $('#m').val('')
+    return false
+  })
+  socket.on('chat message', (msg) => {
+    $('#messages').append($('<li>').text(msg))
+  })
+})
