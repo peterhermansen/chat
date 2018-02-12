c17e1b9b6e644978ad1b79a460cc2654d468c963
Peter Hermansen <hermansenph@gmail.com>
2018-02-12 12:25:48 -0800

    Create express app #1

--- server/create-app.js
+++ server/create-app.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const path = require('path')
 
+function createApp() {
+
+  const app = express()
+
+  app
+    .use(express.static(path.join(__dirname, 'public')))
+
+  return app
+}
+
+module.exports = { createApp }
--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
+const { createApp } = require('./create-app')
 
+const app = createApp()
+
+app.listen(3000, () => {
+  console.log('Listening on :3000')
+})
