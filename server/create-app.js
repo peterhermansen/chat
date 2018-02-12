b91ff07cda5143061134dda9dd1a387174916a30
Peter Hermansen <hermansenph@gmail.com>
2018-02-12 12:55:30 -0800

    Initialize instance of socket.io

--- server/create-app.js
+++ server/create-app.js
@@ -4,9 +4,21 @@ const path = require('path')
 function createApp() {
 
   const app = express()
+  const http = require('http').Server(app)
+  const io = require('socket.io')(http)
 
   app
     .use(express.static(path.join(__dirname, 'public')))
+
+  io
+    .on('connection', (socket) => (
+      console.log('A user has connected')
+    ))
+
+  http
+    .listen(3000, () => {
+      console.log('Listening on :3000')
+    })
 
   return app
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,3 @@
 const { createApp } = require('./create-app')
 
-const app = createApp()
-
-app.listen(3000, () => {
-  console.log('Listening on :3000')
-})
+createApp()
