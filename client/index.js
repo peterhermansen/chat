15a2cb81f7d400fd4866c9714eda85d8e2e48714
Peter Hermansen <hermansenph@gmail.com>
2018-02-12 16:08:04 -0800

    Rendered App to DOM using React #3

--- client/index.js
+++ client/index.js
@@ -1,13 +1,8 @@
-import io from 'socket.io-client'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
 
-$(function () {
-  var socket = io()
-  $('form').submit(() => {
-    socket.emit('chat message', $('#m').val())
-    $('#m').val('')
-    return false
-  })
-  socket.on('chat message', (msg) => {
-    $('#messages').append($('<li>').text(msg))
-  })
-})
+ReactDOM.render(
+  <App />,
+  document.querySelector('#chat')
+)
