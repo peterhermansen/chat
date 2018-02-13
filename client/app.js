d7a1767fe7901f2bf6eb5447ace3a66ba09c6073
Peter Hermansen <hermansenph@gmail.com>
2018-02-12 16:59:27 -0800

    Added functionality to App to send and recieve messages through socketIO #3

--- client/app.js
+++ client/app.js
@@ -1,13 +1,58 @@
 import React from 'react'
+import io from 'socket.io-client'
+const socket = io()
 
 export default class App extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      inputValue: '',
+      messageArray: []
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    socket.on('chat message', (message) => {
+      let messageArray = this.state.messageArray.slice()
+      messageArray.push(message)
+      this.setState({messageArray: messageArray})
+    })
+  }
+
+  handleSubmit(event) {
+    if (this.state.inputValue) {
+      event.preventDefault()
+      socket.emit('chat message', this.state.inputValue)
+      this.setState({inputValue: ''})
+    }
+  }
+
+  handleChange(event) {
+    this.setState({inputValue: event.target.value})
+  }
+
+  renderList(message) {
+    return (
+      <li>{message}</li>
+    )
+  }
+
   render() {
     return (
-      <div id="chat">
-        <ul id="messages"></ul>
-        <form action="">
-          <input id="m" autoComplete="off" /><button>Send</button>
+      <div id="chat-app">
+        <ul id="messages">
+          {this.state.messageArray.map(this.renderList)}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <input id="message-input" autoComplete="off"
+            value={this.state.inputValue}
+            onChange={(event) => this.handleChange(event)}
+          />
+          <button id="message-send">Send</button>
         </form>
       </div>
     )
